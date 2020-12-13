a094c2c1dacae09a44907a82bace0373e2869415
Limegrass <james@niis.me>
2020-12-13 14:03:26 -0800

    converted to warp
    dependencies seem more typical rather
    than being all in the actix space.

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,21 @@
 use clap::ArgMatches;
 use nameof::name_of;
+use std::net::SocketAddr;
 
 #[derive(Debug)]
 pub(crate) struct ServerConfig {
-    pub(crate) address: String,
-    pub(crate) port: u16,
+    pub(crate) addr: SocketAddr,
 }
 
 impl<'a> From<ArgMatches<'a>> for ServerConfig {
     fn from(arg_matches: ArgMatches<'a>) -> Self {
         ServerConfig {
-            address: arg_matches
-                .value_of(name_of!(address in ServerConfig))
+            addr: arg_matches
+                .value_of(name_of!(addr in ServerConfig))
                 .expect("should have defaulted if not provided")
-                .to_owned(),
-            port: arg_matches
-                .value_of(name_of!(port in ServerConfig))
-                .expect("should have defaulted if not provided")
+                .to_owned()
                 .parse()
-                .expect("port should be a number"),
+                .expect("must be a valid socket addr. eg: 127.0.0.1:8080"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,29 @@
 mod config;
 
-use actix_web::{error::ResponseError, middleware, web, App as ServerApp, HttpRequest, HttpServer};
 use clap::{App, Arg};
 use config::ServerConfig;
 use nameof::name_of;
-use std::fmt::Display;
+use warp::Filter;
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {:?}", req);
-    "Hello world!"
-}
-
-#[derive(Debug)]
-struct Error {
-    error: anyhow::Error,
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
-impl ResponseError for Error {}
-
-impl From<anyhow::Error> for Error {
-    fn from(error: anyhow::Error) -> Error {
-        Error { error }
-    }
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() {
     env_logger::init();
     let arg_matches = App::new("Trello Git Webhook")
         .version("0.1.0")
         .author("James N. <james@niis.me>")
         .about("Updates Trello on git push")
         .arg(
-            Arg::with_name(name_of!(address in ServerConfig))
+            Arg::with_name(name_of!(addr in ServerConfig))
                 .short("a")
                 .long("address")
                 .value_name("ADDRESS")
                 .help("ip address to bind to")
                 .takes_value(true)
-                .default_value("127.0.0.1"),
-        )
-        .arg(
-            Arg::with_name(name_of!(port in ServerConfig))
-                .short("p")
-                .long("port")
-                .value_name("PORT")
-                .help("port on the ip address to bind to")
-                .takes_value(true)
-                .default_value("9669"), // TODO: limited range
+                .default_value("127.0.0.1:9669"),
         )
         .get_matches();
     let config = ServerConfig::from(arg_matches);
 
-    HttpServer::new(|| {
-        ServerApp::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
-    })
-    .bind(format!("{}:{}", config.address, config.port))?
-    .run()
-    .await
+    let root = warp::any().map(|| "Hello world!");
+    warp::serve(root).run(config.addr).await;
 }
