7c4bf29ba2f2708227524831624d4b061c616656
Limegrass <james@niis.me>
2020-12-13 21:24:13 -0800

    added allowed_host parameter
    if I'm planning for a CLI route,
    there would otherwise be large risks with executing commands.
    having an allow list can mitigate that if I go that route.
    lazy one host solution for now until I need more.

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,33 @@
 use clap::ArgMatches;
+use log::*;
 use nameof::name_of;
 use std::net::SocketAddr;
 
 #[derive(Debug)]
 pub(crate) struct ServerConfig {
-    pub(crate) addr: SocketAddr,
+    pub addr: SocketAddr,
+    pub allowed_host: String,
 }
 
 impl<'a> From<ArgMatches<'a>> for ServerConfig {
     fn from(arg_matches: ArgMatches<'a>) -> Self {
+        let addr = arg_matches
+            .value_of(name_of!(addr in ServerConfig))
+            .expect("should have defaulted if not provided");
+        info!("{:?}", addr);
+        let addr: SocketAddr = addr
+            .to_owned()
+            .parse()
+            .expect("must be a valid socket addr. eg: 127.0.0.1:8080");
+
+        let allowed_host = arg_matches
+            .value_of(name_of!(allowed_host in ServerConfig))
+            .expect("should have defaulted if not provided");
+        info!("{:?}", allowed_host);
+
         ServerConfig {
-            addr: arg_matches
-                .value_of(name_of!(addr in ServerConfig))
-                .expect("should have defaulted if not provided")
-                .to_owned()
-                .parse()
-                .expect("must be a valid socket addr. eg: 127.0.0.1:8080"),
+            addr,
+            allowed_host: allowed_host.to_owned(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,23 @@ async fn main() {
                 .takes_value(true)
                 .default_value("127.0.0.1:9669"),
         )
+        .arg(
+            Arg::with_name(name_of!(allowed_host in ServerConfig))
+                .short("h")
+                .long("allowed_hosts")
+                .value_name("ALLOWED_HOSTS")
+                .help("host names allowed to send requests")
+                .takes_value(true)
+                .default_value(r#"127.0.0.1:9669"#),
+        )
         .get_matches();
     info!("{:?}", arg_matches);
 
     let config = ServerConfig::from(arg_matches);
     info!("{:?}", config);
+    let host = warp::host::exact(&format!("localhost:{}", config.addr.port()))
+        .or(warp::host::exact(&config.allowed_host));
 
-    let root = warp::any().map(|| "Hello world!");
+    let root = host.map(|_| "Hello world!");
     warp::serve(root).run(config.addr).await;
 }
